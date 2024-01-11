5be861187c2493ccaaaccbc1634bc60e5f8a68e7
Danosaure <1444063+danosaure@users.noreply.github.com>
2024-01-11 01:01:43 -0500

    Modified logo

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,13 @@
 import Grid from '@mui/material/Grid';
 
-import ThemeToggler from '../theme-toggler';
+import ThemeToggler from './theme-toggler';
 
 export default () => (
   <Grid container className="dano-header" alignItems="center" justifyContent="center">
     <Grid item>
         <img className="dano-logo" src="assets/danosaure-logo.png" />
     </Grid>
-    <Grid item>
+    <Grid item sx={{flexGrow: 1}}>
       <h1>MLSpin App</h1>
     </Grid>
     <Grid item>
