40feb12590c285c4385f44eb004339787180c08a
Danosaure <1444063+danosaure@users.noreply.github.com>
2024-01-17 15:55:22 -0500

    Adding tab icons

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,5 +1,9 @@
 import { useState, SyntheticEvent } from 'react';
 
+import BusinessIcon from '@mui/icons-material/Business';
+import ImportExportIcon from '@mui/icons-material/ImportExport';
+import MapIcon from '@mui/icons-material/Map';
+import PersonIcon from '@mui/icons-material/Person';
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
@@ -19,10 +23,11 @@ export default () => {
   return (
     <Box component="main" className="dano-main">
       <Tabs className="dano-tabs" value={tabId} onChange={changeTab} aria-label="Navigation tabs" variant="fullWidth">
-        <Tab className="dano-tabs-item" label="Agents" {...a11yProps(0)} />
-        <Tab className="dano-tabs-item" label="Offices" {...a11yProps(1)} />
+        <Tab className="dano-tabs-item" label="Agents" {...a11yProps(0)} icon={<PersonIcon />} />
+        <Tab className="dano-tabs-item" label="Offices" {...a11yProps(1)} icon={<BusinessIcon />} />
         <Tab className="dano-tabs-item dano-tabs-divider" label="" disabled /> {/* counted as 2 */}
-        <Tab className="dano-tabs-item" label="Imports" {...a11yProps(3)} />
+        <Tab className="dano-tabs-item" label="Zip lookup" {...a11yProps(3)} icon={<MapIcon />} />
+        <Tab className="dano-tabs-item" label="Imports" {...a11yProps(4)} icon={<ImportExportIcon />} />
       </Tabs>
 
       <TabPanel tabId={0} selectedTabId={tabId}>
@@ -34,6 +39,10 @@ export default () => {
       </TabPanel>
 
       <TabPanel tabId={3} selectedTabId={tabId}>
+        Zip form
+      </TabPanel>
+
+      <TabPanel tabId={4} selectedTabId={tabId}>
         <Importers />
       </TabPanel>
     </Box>
