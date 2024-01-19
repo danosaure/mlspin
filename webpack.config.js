f6e812949fa4342bd8197f6e1e3f86d6749e2cfd
Danosaure <1444063+danosaure@users.noreply.github.com>
2024-01-19 00:57:01 -0500

    Swapping out ts-loader in favor of babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,41 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    devtool: 'source-map',
-    cache: false,
-    entry: {
-        app: './src/index.tsx',
-    },
-    output: {
-        path: path.resolve(__dirname, 'docs'),
-        filename: '[name].min.js'
-    },
-    resolve: {
-        extensions: [
-            '.js',
-            '.jsx',
-            '.ts',
-            '.tsx'
-        ]
-    },
-    module: {
-        rules: [{
-            test: /\.(ts|tsx)$/,
-            exclude: /node_modules/,
-            use: 'ts-loader'
-        }]
-    },
-    optimization: {
-        minimize: process.env.NODE_ENV !== 'development'
-    }
+  mode: 'production',
+  devtool: 'source-map',
+  cache: false,
+  entry: {
+    app: path.join(__dirname, 'src', 'index.tsx'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'docs'),
+    filename: '[name].min.js',
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    '@mui/material': 'MaterialUI',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    modules: ['node_modules'],
+  },
+  target: ['web', 'es5'],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+          },
+        },
+      },
+    ],
+  },
+  optimization: {
+    minimize: process.env.NODE_ENV !== 'development',
+  },
 };
