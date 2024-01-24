b43071a37bea8bbc35582f4314d93c7a1fd8eaf4
Danosaure <1444063+danosaure@users.noreply.github.com>
2024-01-24 15:28:07 -0500

    Fixed className undefined

--- src/components/main-panel/main-panel-section/index.tsx
+++ src/components/main-panel/main-panel-section/index.tsx
@@ -1,13 +1,23 @@
-import { ReactNode } from 'react';
 import { Box } from '@mui/material';
+import classnames from 'classnames';
+import { ReactNode } from 'react';
 
 export interface MainPanelSectionProps {
   className?: string;
   children?: ReactNode;
 }
 
-export default ({ children, className }: MainPanelSectionProps) => (
-  <Box className={`dano-main-panel-section ${className}`} component="section">
-    {children}
-  </Box>
-);
+export default ({ children, className }: MainPanelSectionProps) => {
+  const classNames = classnames(
+    'dano-main-panel-section',
+    {
+      [className || '']: Boolean(className),
+    }
+  );
+
+  return (
+    <Box className={classNames} component="section">
+      {children}
+    </Box>
+  );
+};
