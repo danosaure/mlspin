c793dda8a6446acbf03f0830c0bfc1ebd914cd01
Danosaure <1444063+danosaure@users.noreply.github.com>
2024-02-04 17:47:23 -0500

    - #39: Moving USPS data to JSON file to be uploaded instead of pasted into a field.

--- src/components/admin/mlspin-importer.tsx
+++ src/components/admin/mlspin-importer.tsx
@@ -1,30 +1,21 @@
-import { useState } from 'react';
-
-import { AlertColor } from '@mui/material/Alert';
+import { useContext } from 'react';
 
 import { importMLS } from '../../import';
 import MainPanel from '../main-panel';
-import Snackbars from '../snackbars';
 import GenericImporterForm from './generic-importer-form';
-
-type MLSPinImporterSnackType = {
-  severity: AlertColor;
-  message: string;
-};
+import { SnackbarsContext } from '../snackbars-context';
 
 export default () => {
-  const [snackMessages, setSnackMessages] = useState<MLSPinImporterSnackType[]>([]);
-
-  const snackbarClosed = () => setSnackMessages([]);
+  const { addMessage } = useContext(SnackbarsContext);
 
   const saveContent = async (content: string) => {
-    setSnackMessages(snackMessages.concat({ severity: 'warning', message: 'Import in progress...' }));
+    addMessage({ severity: 'warning', message: 'Import in progress...' });
 
     try {
       await importMLS(content);
-      setSnackMessages(snackMessages.concat({ severity: 'success', message: 'Data imported successfully.' }));
+      addMessage({ severity: 'success', message: 'Data imported successfully.' });
     } catch (e) {
-      setSnackMessages(snackMessages.concat({ severity: 'error', message: e instanceof Error ? e.message : String(e) }));
+      addMessage({ severity: 'error', message: e instanceof Error ? e.message : String(e) });
     }
   };
 
@@ -35,8 +26,6 @@ export default () => {
         saveContent={saveContent}
         placeholder="Paste in the output from the bookmarklet."
       />
-
-      <Snackbars snackbarClosed={snackbarClosed} snacks={snackMessages} />
     </MainPanel>
   );
 };
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,10 +2,24 @@ import { Container } from '@mui/material';
 
 import Header from './header';
 import Main from './main';
+import { SnackbarsContext } from './snackbars-context';
+import { useState } from 'react';
+import Snackbars, { MLSPinSnackbarsType } from './snackbars';
 
-export default () => (
-  <Container fixed className="dano-app">
-    <Header />
-    <Main />
-  </Container>
-);
+export default () => {
+  const [messages, setMessages] = useState<MLSPinSnackbarsType[]>([]);
+
+  const addMessage = (message: MLSPinSnackbarsType) => setMessages(messages.concat(message));
+  const clearMessages = () => setMessages([]);
+
+  return (
+    <SnackbarsContext.Provider value={{ addMessage, clearMessages }}>
+      <Container fixed className="dano-app">
+        <Header />
+        <Main />
+      </Container>
+
+      <Snackbars snackbarClosed={clearMessages} snacks={messages} />
+    </SnackbarsContext.Provider>
+  );
+};
