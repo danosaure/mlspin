1fc1e5e53a9fec703e1318cb4efa72cf8ff91d4e
Danosaure <1444063+danosaure@users.noreply.github.com>
2024-07-15 01:15:35 -0400

    #47: Adding tab for IDX.

--- src/components/admin/index.tsx
+++ src/components/admin/index.tsx
@@ -1,4 +1,9 @@
-import { Dataset as DatasetIcon, ImportExport as ImportExportIcon, Map as MapIcon } from '@mui/icons-material';
+import {
+  Dataset as DatasetIcon,
+  ImportExport as ImportExportIcon,
+  Map as MapIcon,
+  TableView as TableViewIcon,
+} from '@mui/icons-material';
 import { Box, Stack, Tab, Tabs, Typography } from '@mui/material';
 import { SyntheticEvent } from 'react';
 
@@ -26,6 +31,7 @@ const MLSPinAdmin = () => {
           <Tab className="dano--admin--tabs--item" label="Zip" {...a11yProps('zip')} icon={<MapIcon />} />
           <Tab className="dano--admin--tabs--item dano--admin--tabs--divider" label="" disabled />
           <Tab className="dano--admin--tabs--item" label="MLSpin" {...a11yProps('mls')} icon={<DatasetIcon />} />
+          <Tab className="dano--admin--tabs--item" label="IDX" {...a11yProps('idx')} icon={<TableViewIcon />} />
           <Tab className="dano--admin--tabs--item" label="Backups" {...a11yProps('backups')} icon={<ImportExportIcon />} />
         </Tabs>
       </Stack>
@@ -36,6 +42,10 @@ const MLSPinAdmin = () => {
 
       <TabPanel tabId="zip" selectedTabId={tabId}>
         <AdminZipLookup />
+      </TabPanel>
+
+      <TabPanel tabId="idx" selectedTabId={tabId}>
+        TODO...
       </TabPanel>
 
       <TabPanel tabId="backups" selectedTabId={tabId}>
