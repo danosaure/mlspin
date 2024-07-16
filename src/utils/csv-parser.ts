235d0997f45295cfeb2ccaf5e5618fdd2b441807
Danosaure <1444063+danosaure@users.noreply.github.com>
2024-07-16 01:10:29 -0400

    Extended to allow custom separator

--- src/utils/csv-parser.ts
+++ src/utils/csv-parser.ts
@@ -1,20 +1,20 @@
-import { CSVParsedType } from './csv-parsed-type';
+import type { CSVParsedType } from './csv-parsed-type';
 
 const SEPARATOR = ';';
 const MATCHING_QUOTES = /^(['"]).*\1$/;
 
 const cleanValue = (s: string) => (MATCHING_QUOTES.test(s) ? s.substring(1, s.length - 1) : s);
 
-export const parseCSV = (content: string): CSVParsedType[] => {
+export const parseCSV = (content: string, separator?: string): CSVParsedType[] => {
   const results: CSVParsedType[] = [];
 
   const lines = content.trim().split('\n');
 
   if (lines.length > 1) {
-    const headers = lines[0].split(SEPARATOR);
+    const headers = lines[0].split(separator || SEPARATOR);
 
     lines.slice(1).forEach((line: string) => {
-      const values = line.split(SEPARATOR);
+      const values = line.split(separator || SEPARATOR);
       const data: CSVParsedType = headers.reduce(
         (obj, attr, idx) => ({
           ...obj,
--- src/utils/csv-parser.unit.test.ts
+++ src/utils/csv-parser.unit.test.ts
@@ -11,4 +11,59 @@ describe(namespace(path.basename(__filename)), () => {
     const parsed: CSVParsedType[] = parseCSV('');
     expect(parsed).to.deep.equal([]);
   });
+
+  describe('No separator params', () => {
+    it('Basic use', () => {
+      const parsed: CSVParsedType[] = parseCSV('H1;H2\nv1;v2');
+      expect(parsed).to.deep.equal([
+        {
+          H1: 'v1',
+          H2: 'v2',
+        },
+      ]);
+    });
+
+    it('Do no split on wrong separator', () => {
+      const parsed: CSVParsedType[] = parseCSV('H1|H2\nv1|v2');
+      expect(parsed).to.deep.equal([
+        {
+          'H1|H2': 'v1|v2',
+        },
+      ]);
+    });
+
+    it('Split on second record', () => {
+      const parsed: CSVParsedType[] = parseCSV('H1|H2\nv1|v2\nv3;v4');
+      expect(parsed).to.deep.equal([
+        {
+          'H1|H2': 'v1|v2',
+        },
+        {
+          'H1|H2': 'v3',
+        },
+      ]);
+    });
+  });
+
+  describe('With separator param', () => {
+    it('Basic use', () => {
+      const parsed = parseCSV('H1|H2\nv1|v2', '|');
+      expect(parsed).to.deep.equal([
+        {
+          H1: 'v1',
+          H2: 'v2',
+        },
+      ]);
+    });
+
+    it('Ignores the default separator', () => {
+      const parsed = parseCSV('H1|H2\nv1;v2', '|');
+      expect(parsed).to.deep.equal([
+        {
+          H1: 'v1;v2',
+          H2: '',
+        },
+      ]);
+    });
+  });
 });
