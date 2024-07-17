f398a41d05b81f59d741ae995de8f6ce8c3ad570
Danosaure <1444063+danosaure@users.noreply.github.com>
2024-07-17 10:22:37 -0400

    - #47: Handling incomplete file due to server time out error message.

--- src/import/idx/import-idx-listings.ts
+++ src/import/idx/import-idx-listings.ts
@@ -15,7 +15,19 @@ const importIDXListings = async (filename: string, content: CSVParsedType[]): Pr
 
   // TODO: Filter for existing agents and offices only.
 
-  await putManyRaw(objectStore, content);
+  // This filters out an incomplete file with the error at the bottom:
+  //    <font face="Arial" size=2>
+  //      <p>Active Server Pages</font> <font face="Arial" size=2>error 'ASP 0113'</font>
+  //      <p>
+  //      <font face="Arial" size=2>Script timed out</font>
+  //      <p>
+  //      <font face="Arial" size=2>/idx.asp</font>
+  //      <p>
+  //      <font face="Arial" size=2>The maximum amount of time for a script to execute was exceeded. You can change this limit by specifying a new value for the property Server.ScriptTimeout or by changing the value in the IIS administration tools.
+  //      </font>
+  const nonEmptyContent: CSVParsedType[] = content.filter((item: CSVParsedType) => item.PROP_TYPE && item.STATUS);
+
+  await putManyRaw(objectStore, nonEmptyContent);
 
   transaction.complete();
 };
